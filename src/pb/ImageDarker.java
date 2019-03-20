ff25dfcf59d0f428c6f061869fa71d3cfcf19754
Mateusz <wojno.mateusz@gmail.com>
2019-03-20 17:59:02 +0100

    Fixed method makeDarker() in ImageDarker class

--- src/pb/ImageDarker.java
+++ src/pb/ImageDarker.java
@@ -1,14 +1,10 @@
 package pb;
 
-import javafx.scene.effect.ColorAdjust;
-import javafx.scene.image.*;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
-
-import java.awt.*;
-import java.nio.Buffer;
-import java.nio.ByteBuffer;
-import java.nio.IntBuffer;
 
 public class ImageDarker {
 
@@ -19,64 +15,60 @@ public class ImageDarker {
     private long red[] = new long[256];
     private long green[] = new long[256];
     private long blue[] = new long[256];
-    private double brightestRed=0;
-    private double brightestGreen=0;
-    private double brightestBlue=0;
+    private double brightestRed = 0;
+    private double brightestGreen = 0;
+    private double brightestBlue = 0;
     private boolean success;
 
-    ImageDarker(WritableImage src)
-    {
+    ImageDarker(WritableImage src) {
         copyImage = src;
-        success=false;
+        success = false;
     }
 
-    public Image makeDarker(){
+    public Image makeDarker() {
         PixelReader pixelReader = copyImage.getPixelReader();
 
         PixelWriter pw = copyImage.getPixelWriter();
+        for (int x = 0; x < copyImage.getWidth(); x++) {
+            for (int y = 0; y < copyImage.getHeight(); y++) {
 
-        for (int y = 0; y < copyImage.getHeight(); y++) {
-            for (int x = 0; x < copyImage.getWidth(); x++) {
+                java.awt.Color color = new java.awt.Color(pixelReader.getArgb(x, y));
 
-                Color color =  copyImage.getPixelReader().getColor(x,y);
-
-                if(color.getRed()>brightestRed)
-                    brightestRed=color.getRed();
-                if(color.getBlue()>brightestBlue)
-                    brightestBlue=color.getBlue();
-                if(color.getGreen()>brightestGreen)
-                    brightestGreen=color.getGreen();
+                if (color.getRed() > brightestRed)
+                    brightestRed = color.getRed();
+                if (color.getBlue() > brightestBlue)
+                    brightestBlue = color.getBlue();
+                if (color.getGreen() > brightestGreen)
+                    brightestGreen = color.getGreen();
 
             }
         }
-        System.out.println("brightestREdd:" + brightestRed + "brightestGreen:" + brightestGreen );
+        System.out.println("brightestREdd:" + brightestRed + "brightestGreen:" + brightestGreen);
 
-        brightestRed = 255 / Math.pow(brightestRed,2);
-        brightestBlue = 255 / Math.pow(brightestBlue,2);
-        brightestGreen = 255 / Math.pow(brightestGreen,2);
+        brightestRed = 255 / Math.pow(brightestRed, 2);
+        brightestBlue = 255 / Math.pow(brightestBlue, 2);
+        brightestGreen = 255 / Math.pow(brightestGreen, 2);
 
 
-
-        for(int i=0; i<256; i++)
-        {
-            red[i] = (long) (brightestRed * Math.pow(brightestRed,2));
-            green[i] = (long) (brightestGreen * Math.pow(brightestGreen,2));
-            blue[i] = (long) (brightestBlue * Math.pow(brightestBlue,2));
+        for (int i = 0; i < 256; i++) {
+            red[i] = (long) (brightestRed * Math.pow(i, 2));
+            green[i] = (long) (brightestGreen * Math.pow(i, 2));
+            blue[i] = (long) (brightestBlue * Math.pow(i, 2));
         }
 
-        for (int y = 0; y < copyImage.getHeight(); y++) {
-            for (int x = 0; x < copyImage.getWidth(); x++) {
+        for (int x = 0; x < copyImage.getWidth(); x++) {
+            for (int y = 0; y < copyImage.getHeight(); y++) {
 
-                Color color =  copyImage.getPixelReader().getColor(x,y);
+                java.awt.Color color = new java.awt.Color(pixelReader.getArgb(x, y));
 
-                int r = (int) red[(int) color.getRed()];
-                int g = (int) green[(int) color.getGreen()];
-                int b = (int) blue[(int) color.getBlue()];
+                int r = (int) red[color.getRed()];
+                int g = (int) green[color.getGreen()];
+                int b = (int) blue[color.getBlue()];
 
-                System.out.println("red" + r + "green:" + g );
+                System.out.println("red" + r + "green:" + g);
 
-                Color newColor = Color.rgb(r,g,b);
-                pw.setColor(x,y,newColor);
+                Color newColor = Color.rgb(r, g, b);
+                pw.setColor(x, y, newColor);
 
             }
         }
